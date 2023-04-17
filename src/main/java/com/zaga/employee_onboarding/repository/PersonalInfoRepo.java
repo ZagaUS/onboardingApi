bcb584075708b60c57449950942896a29cdfd758
Raga2707 <ragamalya295@gmail.com>
2023-04-17 12:39:21 +0530

    changed to Mongo cluster

--- src/main/java/com/zaga/employee_onboarding/repository/PersonalInfoRepo.java
+++ src/main/java/com/zaga/employee_onboarding/repository/PersonalInfoRepo.java
@@ -4,17 +4,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.zaga.employee_onboarding.entity.dto.PersonalInfo;
+import com.zaga.employee_onboarding.entity.PersonalInfo;
 
-@Repository
 public interface PersonalInfoRepo  extends MongoRepository<PersonalInfo, String>{
 
     @Query(value ="{'employeeId' : ?0}")
     public PersonalInfo getPersonalnfo(String employeeId);
-    
-
-  
-
     
 } 
     
