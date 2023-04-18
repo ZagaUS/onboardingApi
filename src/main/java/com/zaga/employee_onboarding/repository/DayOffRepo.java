19ad9cbd6d3d8b357a580f98c44bfd185e3036c9
Jeyaraghul <jeyaraghul@gmail.com>
2023-04-18 15:12:16 +0530

    fixed merge conflicts

--- src/main/java/com/zaga/employee_onboarding/repository/DayOffRepo.java
+++ src/main/java/com/zaga/employee_onboarding/repository/DayOffRepo.java
@@ -4,10 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.zaga.employee_onboarding.entity.DayOff;
-<<<<<<< HEAD
 
-=======
->>>>>>> 96f8e890f7ef99bdd407e89ab0510600c524af44
 @Repository
 public interface DayOffRepo extends MongoRepository<DayOff, String> {
 
