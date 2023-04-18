e2c0ea6578ad27268df992c2fd51386855cd837a
Jeyaraghul <jeyaraghul@gmail.com>
2023-04-18 15:28:22 +0530

    Added EmployeeDto for aggregation

---
+++ src/main/java/com/zaga/employee_onboarding/entity/dto/EmployeeInfoDto.java
@@ -0,0 +1,17 @@
+package com.zaga.employee_onboarding.entity.dto;
+
+public class EmployeeInfoDto {
+    public String employeeId;
+    public String employeeName;
+    public String employeeRole;
+    public String jobTitle;
+    public String dateOfJoining;
+    public String employeeEmail;
+    public String password;
+    public String department;
+    public String reportingManager;
+    public String employeeStatus;
+    public Integer overallExperience;
+    public Boolean projectAssignmentStatus;
+
+}
