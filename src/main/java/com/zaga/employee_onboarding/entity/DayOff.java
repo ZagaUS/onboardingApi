9cd0d616afd6c80708ce0ab2777864159a6228a7
pavithra200595 <pavipavi491@gmail.com>
2023-04-20 22:28:07 +0530

    Wrote IntegrationTest for createDayOff and getDayOffById in DayOff API

--- src/main/java/com/zaga/employee_onboarding/entity/DayOff.java
+++ src/main/java/com/zaga/employee_onboarding/entity/DayOff.java
@@ -4,10 +4,12 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "dayoff")
---
+++ src/test/java/com/zaga/employee_onboarding/DayOffAPITest.java
@@ -0,0 +1,75 @@
+package com.zaga.employee_onboarding;
+
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.zaga.employee_onboarding.entity.DayOff;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class DayOffAPITest {
+
+    private DayOff dayOffTest;
+
+    @LocalServerPort
+    int port;
+
+    @Test
+    @Order(1)
+
+    public void createDayOffTest() throws Exception{
+        DayOff df = DayOff.builder().employeeId("1").employeeName("Pavithra").allocatedLeave("10").used("8").balance("2").build();
+        dayOffTest=RestAssured.given()
+                .baseUri("http://localhost")
+                .port(port).contentType(ContentType.JSON).accept(ContentType.JSON).body(df).when()
+                .post("/createDayOff")
+                .then()
+                .statusCode(200).extract().as(DayOff.class);
+        System.out.println(dayOffTest);
+
+
+       }
+
+       @Test
+       @Order(2)
+       public void getDayOffById()
+       {
+        RestAssured.given().baseUri("http://localhost")
+        .port(port)
+        .contentType(ContentType.JSON)
+        .queryParam("employeeId", dayOffTest.employeeId)
+        .when()
+        .get("/getDayOffById")
+        .then()
+        .statusCode(200);
+    }
+
+    @Test
+    @Order(3)
+    public void updateDayOffTest() throws Exception{
+        DayOff updateDF = DayOff.builder().employeeName("Pavithra").allocatedLeave("15").build();
+        // DayOff updateDF = DayOff.builder().allocatedLeave("15").used("8").balance("7").build();
+        RestAssured.given().baseUri("http://localhost").port(port).contentType(ContentType.JSON)
+        //.queryParam("employeeId", dayOffTest.employeeId)
+        .accept(ContentType.JSON).body(updateDF)
+        .when().put("/updateDayOff")
+        .then();
+        //.statusCode(200);
+    }
+}
+
