570d3305ef410459afb648b65e01ffb381987c4f
Raga2707 <ragamalya295@gmail.com>
2023-04-29 19:33:58 +0530

    Modified TrainingDetails

--- src/main/java/com/zaga/employee_onboarding/entity/TaskDTO.java
+++ src/main/java/com/zaga/employee_onboarding/entity/TaskDTO.java
@@ -1,10 +1,6 @@
 package com.zaga.employee_onboarding.entity;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,16 +9,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonIgnoreProperties({"id"})
-@Document(collection = "Task")
 
-public class Task {
-    
-   
+public class TaskDTO {
+
     @Id
-    public ObjectId id;
     public String taskId;
     public String taskName;
     public String description;
-    public Status status; 
+    public Status status;
+    
 }
--- src/main/java/com/zaga/employee_onboarding/entity/TrainingDetails.java
+++ src/main/java/com/zaga/employee_onboarding/entity/TrainingDetails.java
@@ -1,10 +1,9 @@
 package com.zaga.employee_onboarding.entity;
 
-import org.bson.types.ObjectId;
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,16 +12,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonIgnoreProperties({"id"})
 @Document(collection = "trainingDetails")
 public class TrainingDetails {
+    
     @Id
-    public ObjectId id;
     public String trainingId;
     public String trainingName;
     public String trainerName;
     public String traineeName;
     public String trainingStartDate;
     public String trainingEndDate;
+    public List<TaskDTO> taskList;
 
 }
