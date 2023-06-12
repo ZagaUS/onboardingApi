26bc4b90b3f39d2b0d231646711f526c8a3d7343
Raga2707 <ragamalya295@gmail.com>
2023-06-12 16:51:36 +0530

    Modified Cross-Origin

--- src/main/java/com/zaga/employee_onboarding/EmployeeOnboardingApplication.java
+++ src/main/java/com/zaga/employee_onboarding/EmployeeOnboardingApplication.java
@@ -2,12 +2,29 @@ package com.zaga.employee_onboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeeOnboardingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeOnboardingApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/zaga/employeeOnboarding**")
+            .allowedOrigins("http://localhost:8090")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Authorization", "Content-Type", "X-Requested-With")
+            .allowCredentials(false);
+			}
+		};
 	}
 
 }
