cd4538dd4f584ae95e268476c1a199917a41b62a
Raga2707 <ragamalya295@gmail.com>
2023-07-05 14:49:44 +0530

    Modified EducationDetails

--- src/main/java/com/zaga/employee_onboarding/serviceimpl/EducationDetailsServiceImpl.java
+++ src/main/java/com/zaga/employee_onboarding/serviceimpl/EducationDetailsServiceImpl.java
@@ -18,7 +18,7 @@ public class EducationDetailsServiceImpl implements EducationDetailsService {
     @Override
     public EducationDetails createEducationalInfo(EducationDetails dto) {
         EducationDetails info = repo.save(dto);
-        System.out.println("Educational Info Created");
+        System.out.println("Created Educational Info");
         return info;
     }
 
